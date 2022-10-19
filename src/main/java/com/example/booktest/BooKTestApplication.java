4f3aad913815489c232eb164cc35278d0efc1d7d
Anastasiyu <asy.safiulina@mail.ru>
2022-10-19 23:07:11 +0300

    Initial commit

---
+++ src/main/java/com/example/booktest/BooKTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.booktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooKTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooKTestApplication.class, args);
+    }
+
+}
